46a6297d883d3bb966bf30633d25016fb26c43dd
HelioA <70294456+HelioA@users.noreply.github.com>
2021-05-19 17:24:04 -0300

    Semana 05
    Inicio Frontend - Teste contador

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import {Hello} from './components/Hello';
+import {Counter} from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       <h1>Frontend My Activies Space</h1>
+      <p>Teste prático - Contador</p>
+      <Hello name="Helio"/>
+      <Counter />
     </div>
   );
 }
